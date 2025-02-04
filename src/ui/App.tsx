8eedbf35a4fe85cfe5ce17d9e8bbaad2327a1053
BorakL <lborak88@gmail.com>
2025-02-04 20:55:38 +0100

    added form at app

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,8 +1,27 @@
 import { useEffect } from 'react' 
 import './App.css'
 import klinike from "./../../data/klinike.json";
+import {useForm} from 'react-hook-form';
 
 function App() {
+  type FormValues = {
+    category: 1 | 2 | 3,
+    date: string,
+    session: string
+  }
+  const form = useForm<FormValues>({
+    defaultValues: {
+      category: 1,
+      date: "",
+      session: ""
+    }
+  });
+  const {register, handleSubmit, formState, getValues} = form;
+  const {errors} = formState;
+
+  const onSubmit = () => {
+    downloadShippingDocs()
+  }
  
   useEffect(()=>{
     
@@ -10,21 +29,17 @@ function App() {
 
   const downloadShippingDocs = async () => {
     const refererUrl = "https://prochef.rs/hospital/otpremnice.php"
-    const url = "https://prochef.rs/hospital/create_pdf_invoice_otpremnica_v1.php"
-    const category = 2;
-    const date = "12-01-2025";
-    const session = "107su6vf0qtb03ggkh9rr2rt0a"
+    const url = "https://prochef.rs/hospital/create_pdf_invoice_otpremnica_v1.php" 
+    const {category, date, session} = getValues();
     try{
-      await window.electronApp.createFullFolder(
-        {
+      await window.electronApp.createFullFolder({
           cliniks: klinike,
           url,
           refererUrl,
           category,
           date,
           session
-        }
-      )
+        })
     }catch(error){
       console.error("Greška pri pozivanju createFullFolder:", error)
     }
@@ -33,25 +48,55 @@ function App() {
   return (
     <div className='App'>
       <div>
-        <form action="">
+        <form onSubmit = {handleSubmit(onSubmit)}>
           <div>
-            <label htmlFor="category">Obrok: </label>
-            <input type="number" name="category" id="category" />
+            <label htmlFor="category">Obrok: </label> 
+            <select {...register("category", {
+                required: {
+                  value: true,
+                  message: "Izaberi obrok"
+                },
+                valueAsNumber: true,
+                validate: value => [1,2,3].includes(value)  || 'Izaberi validan obrok'
+              })}
+            >
+              <option value={1}>Doručak</option>
+              <option value={2}>Ručak</option>
+              <option value={3}>Večera</option>
+            </select>
+            <p>{errors.category?.message}</p>
           </div>
           <div>
             <label htmlFor="data">Datum: </label>
-            <input type="date" name="date" id="date" /> 
+            <input 
+              type="date" 
+              id="date" 
+              {...register("date", {
+                required: {
+                  value: true,
+                  message: "Selektujt datum"
+                }
+              })} />
+              <p>{errors.date?.message}</p> 
           </div>      
           <div>
             <label htmlFor="session">PHPSESSID: </label>
-            <input type="text" name="session" id="session" />
-          </div>      
+            <input 
+              type="text" 
+              id="session" 
+              {...register("session", {
+                required: {
+                  value: true,
+                  message: "Unesi PHPSESSID"
+                }
+              })} />
+              <p>{errors.session?.message}</p>
+          </div>
+          <div>
+            <button>Downladuj otrepnice</button>
+          </div>
         </form>
-      </div>
-      <div className="read-the-docs">
-        <button onClick={downloadShippingDocs}>Download shipping docs</button>
       </div> 
-      
     </div>
   )
 }
